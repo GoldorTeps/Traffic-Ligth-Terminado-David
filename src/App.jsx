ca149d4ed6b100a1c005b5f8b40fd032207b7c53
Goldorteps <agolfofree@hotmail.com>
2023-08-22 21:00:14 +0200

    Agrega estilos de Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Light from './components/Ligth'
+import './components/Ligth.css'; 
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+   
+				<div>
+					<Light/>
+				</div>
+
+    </>
+  )
+}
+
+export default App
